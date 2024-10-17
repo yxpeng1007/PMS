b5ea2f2e9d8df72ccd17e06f09fa36dcba22f5f9
felix-lhz <1175906605@qq.com>
2024-10-17 10:25:21 +0800

    init

--- PMS/urls.py
+++ PMS/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('home/', views.home, name='home'),
 ]
