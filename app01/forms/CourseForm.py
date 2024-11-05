53485f521902d51b468d2aa90732696924d28804
yxpeng1007 <yxpeng1007@outlook.com>
2024-11-05 13:12:25 +0800

    Update CourseForm.py

--- app01/forms/CourseForm.py
+++ app01/forms/CourseForm.py
@@ -33,7 +33,19 @@ class CourseForm(forms.ModelForm):
         (8, "8th Period"),
     ]
 
+    COLOR_CHOICES = [
+        ('#FF0000', 'Red'),
+        ('#33FF57', 'Green'),
+        ('#0066FF', 'Blue'),
+        ('#FFFF00', 'Yellow'),
+        ('#FF33FF', 'Pink'),
+        ('#33FFFF', 'Cyan'),
+        ('#FFFFFF', 'White'),
+        # 添加其他颜色选项
+    ]
+
     day_of_week = forms.ChoiceField(
         choices=DAY_OF_WEEK_CHOICES, label="Day of the Week"
     )
     time_slot = forms.ChoiceField(choices=TIME_SLOT_CHOICES, label="Time Slot")
+    color = forms.ChoiceField(choices=COLOR_CHOICES, label='Color')
